4bbe1fd08d77526c94e1e038b0f01c622bdb6b2c
Titouan <titouanphilippe57@gmail.com>
2026-03-29 23:38:42 +0100

    Before rebuilding

---
+++ Library.MVC/ViewModels/CreateInspectionViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Library.Domain;
+
+namespace Library.MVC.ViewModels
+{
+    public class CreateInspectionViewModel
+    {
+        [Required]
+        [Display(Name = "Premises")]
+        public int PremisesId { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        [Display(Name = "Inspection Date")]
+        public DateTime InspectionDate { get; set; } = DateTime.Today;
+
+        [Required]
+        [Range(0, 100)]
+        public int Score { get; set; }
+
+        [Required]
+        public Outcome Outcome { get; set; }
+
+        [Display(Name = "Notes")]
+        public string Notes { get; set; }
+
+        public List<SelectListItem> PremisesList { get; set; } = new List<SelectListItem>();
+    }
+}
